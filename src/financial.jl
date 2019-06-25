7c578bdd8a650d6d255a024e783a86cdf02199fa
Samuel Massinon <samuel.massinon@invenia.ca>
2019-06-25 14:41:47 +0000

    `expected_shortfall` method

---
+++ src/financial.jl
@@ -0,0 +1,27 @@
+"""
+    expected_shortfall(returns, α::Float64)
+
+Calculate the expected shortfall `-𝔼[ r_p | r_p ≤ q_risk_level(r_p) ]`, where `r_p` is
+the portfolio return and `q_risk_level(r_p)` is the `α`-quantile of the
+distribution of `r_p`.
+
+NOTE: Expected shortfall is the _negative_ of the average of the bottom
+`α`-quantile of returns. Assuming average is positive for all `α`, then
+it is good to _minimise_ expected shortfall.
+"""
+function expected_shortfall(returns, α::Float64)
+
+    0 < α < 1 || throw(ArgumentError("α=$α is not between 0 and 1."))
+
+    last_index = floor(Int, α * length(returns))
+    last_index > 0 || throw(
+        ArgumentError(
+        string(
+                "length(returns)=$(length(returns)) not enough elemnts for α=$α.",
+                " Min length(returns)=$(ceil(Int, 1/α))"
+            )
+        )
+    )
+
+    return mean(partialsort(returns, 1:last_index))
+end
