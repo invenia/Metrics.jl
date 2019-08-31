ce1a5fa68e6b0bc81a51d2f2e184b215172abbd0
glenn.moynihan <glenn.moynihan@invenialabs.co.uk>
2019-08-31 20:43:20 +0100

    regression dispatch for distributions

--- src/regression.jl
+++ src/regression.jl
@@ -11,6 +11,8 @@ end
 obs_arrangement(::typeof(squared_error)) = SingleObs()
 const se = squared_error
 
+se(y_true, y_pred::Distribution) = se(y_true, mean(y_pred))
+se(y_true::Distribution, y_pred) = se(y_pred, y_true)
 
 """
     mean_squared_error(y_true, y_pred) -> Float64
@@ -24,6 +26,10 @@ end
 
 obs_arrangement(::typeof(mean_squared_error)) = IteratorOfObs()
 const mse = mean_squared_error
+
+mse(y_true, y_pred::AbstractArray{<:Distribution}) = mse(y_true, mean.(y_pred))
+mse(y_true::AbstractArray{<:Distribution}, y_pred) = mse(y_pred, y_true)
+
 
 """
     root_mean_squared_error(y_true, y_pred) -> Float64
@@ -56,6 +62,7 @@ function normalised_root_mean_squared_error(y_true, y_pred, α::Float64)
     return root_mean_squared_error(y_true, y_pred) /
         (quantile(y_trues, .5 + α) - quantile(y_trues, .5 - α))
 end
+
 obs_arrangement(::typeof(normalised_root_mean_squared_error)) = IteratorOfObs()
 const nrmse = normalised_root_mean_squared_error
 
@@ -80,20 +87,27 @@ function absolute_error(y_true, y_pred)
     @_dimcheck size(y_true) == size(y_pred)
     return sum(abs.(y_true .- y_pred))
 end
+
 obs_arrangement(::typeof(absolute_error)) = SingleObs()
 const ae = absolute_error
+
+ae(y_true, y_pred::Distribution) = ae(y_true,  mean(y_pred))
+ae(y_true::Distribution, y_pred) = ae(y_pred, y_true)
 
 """
     mean_absolute_error(y_true, y_pred) -> Float64
 
 Compute the mean absolute error between a set of truths `y_true` and predictions `y_pred`.
 """
-function mean_absolute_error(y_true::AbstractVector, y_pred::AbstractVector)
+function mean_absolute_error(y_true, y_pred)
     @_dimcheck size(y_true) == size(y_pred)
     return mean(absolute_error.(y_true, y_pred))
 end
 obs_arrangement(::typeof(mean_absolute_error)) = IteratorOfObs()
 const mae = mean_absolute_error
+
+mae(y_true, y_pred::AbstractArray{<:Distribution}) = mae(y_true, mean.(y_pred))
+mae(y_true::AbstractArray{<:Distribution}, y_pred) = mae(y_pred, y_true)
 
 """
     marginal_loglikelihood(dist::Distribution{Univariate}, y_pred) -> Float64
