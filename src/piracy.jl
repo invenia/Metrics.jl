73762829a0343e2b455e3ef091b4737ae1c33765
Bella Wu <bella.wu@invenialabs.co.uk>
2021-02-17 15:36:15 +0800

    add docstring and tighten the test

--- src/piracy.jl
+++ src/piracy.jl
@@ -9,8 +9,14 @@ import Distributions: dof
 """
     GenericTDist{T<:Real}(df::T, μ::T, σ::T)
 
-the TDist provided by Distributions.jl doesn't have non-standard location and scale
-parameter, and hence define our own type.
+Univariate T distribution with location and scale parameters. The reason we have to define
+this type is because that the TDist provided by Distributions.jl doesn't have non-standard
+location and scale parameter.
+
+# Arguments
+- `df::Real`: the degree of freedom
+- `μ::Real`: the location parameter
+- `σ::Real`: the scale parameter
 """
 struct GenericTDist{T<:Real} <: ContinuousUnivariateDistribution
     df::T
@@ -20,7 +26,7 @@ struct GenericTDist{T<:Real} <: ContinuousUnivariateDistribution
 end
 
 function GenericTDist(df::T, μ::T, σ::T; check_args=true) where {T <: Real}
-    check_args && @check_args(GenericTDist, σ >= zero(σ))
+    check_args && @check_args(GenericTDist, (σ >= zero(σ)) && (df > zero(df)))
     return GenericTDist{T}(df, μ, σ)
 end
 
